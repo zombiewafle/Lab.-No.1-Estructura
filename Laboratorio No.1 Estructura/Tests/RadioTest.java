63ef9cb036245759ee0282198c2b0b42a397dbb6
Javier Salazar <40800436+zombiewafle@users.noreply.github.com>
2019-01-18 16:57:12 -0600

    Add files via upload

---
+++ Laboratorio No.1 Estructura/Tests/RadioTest.java
@@ -0,0 +1,66 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RadioTest {
+
+    @Test
+    public void isTurnedOn() {
+    }
+
+    @Test
+    public void turnOn() {
+    }
+
+    @Test
+    public void turnOff() {
+    }
+
+    @Test
+    public void isInFM() {
+    }
+
+    @Test
+    public void changeToFM() {
+    }
+
+    @Test
+    public void changeToAM() {
+    }
+
+    @Test
+    public void getVolume() {
+    }
+
+    @Test
+    public void volumeUp() {
+    }
+
+    @Test
+    public void volumeDown() {
+    }
+
+    @Test
+    public void getStation() {
+    }
+
+    @Test
+    public void stationUp() {
+    }
+
+    @Test
+    public void displayFavoriteStations() {
+    }
+
+    @Test
+    public void favoriteStations() {
+    }
+
+    @Test
+    public void eliminateFavoriteStation() {
+    }
+
+    @Test
+    public void stationDown() {
+    }
+}
